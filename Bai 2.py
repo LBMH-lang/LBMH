32dd437c2b763b4d78450b7ba9c5b3baa4179c72
lebaminhhuy <lebaminhhuy@gmail.com>
2021-01-05 13:39:45 +0700

    LBMH

---
+++ Bai 2.py
@@ -0,0 +1,14 @@
+A=float(input("A="))
+a=float(input("a="))
+if A==0:
+    a==0
+    print("phuong trinh vo so nghiem")
+else:
+    x=-a/A
+    counter=1
+    n=int(input("n="))
+    while counter <= n:
+        print("phuong trinh co nghiem la",x,counter)
+counter+=1
+print("ket thuc")
+
